ae96974e2a489697e6f23ebe4cc081d1ab4f3739
mathahar <mathahar@paypal.com>
2014-07-08 14:41:04 -0700

    commented jquery references

--- public/js/app.js
+++ public/js/app.js
@@ -3,12 +3,12 @@
 
 requirejs.config({
     paths: {
-    	"jquery": "/components/jquery/dist/jquery"    	
+    	// "jquery": "/components/jquery/dist/jquery"    	
     }
 });
 
 
-require(["jquery"], function ($) {
+require([], function ($) {
 
     var app = {
         initialize: function () {
@@ -17,14 +17,14 @@ require(["jquery"], function ($) {
             // FIXME : Move this code or use a better plugin
             // Simple jQuery snippet to convert form labels into inline placeholders
             // Use this : http://webdesign.tutsplus.com/articles/implementing-the-float-label-form-pattern--webdesign-16407
-     	      $("form :input").each(function(index, elem) {
-			    var eId = $(elem).attr("id");
-			    var label = null;
-			    if (eId && (label = $(elem).parents("form").find("label[for="+eId+"]")).length == 1) {
-			        $(elem).attr("placeholder", $(label).html());
-			        $(label).remove();
-			    }
-			 });
+    //  	      $("form :input").each(function(index, elem) {
+			 //    var eId = $(elem).attr("id");
+			 //    var label = null;
+			 //    if (eId && (label = $(elem).parents("form").find("label[for="+eId+"]")).length == 1) {
+			 //        $(elem).attr("placeholder", $(label).html());
+			 //        $(label).remove();
+			 //    }
+			 // });
 
         }
     };
