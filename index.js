4577cc2372d64360a9d23eaece1b284c2b84b503
mathahar <mathahar@paypal.com>
2014-07-19 18:52:31 -0700

    user authentication with Mongodb/Passport local strategy

--- index.js
+++ index.js
@@ -4,12 +4,13 @@
 var kraken = require('kraken-js'),
     app = require('express')(),
     options = require('./lib/spec')(app),
+    userLib = require('./lib/user')(),
     port = process.env.PORT || 8000;
+
 
 
 app.use(kraken(options));
 
-
-app.listen(port, function (err) {
+app.listen(port, function(err) {
     console.log('[%s] Listening on http://localhost:%d', app.settings.env, port);
 });
