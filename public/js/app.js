f0933c1c1d44e95b51ff19dc0f90fbc40320cf4b
mathahar <mathahar@paypal.com>
2014-09-04 14:10:56 -0700

    typeahead feature on profile page

--- public/js/app.js
+++ public/js/app.js
@@ -1,54 +1,89 @@
-'use strict';
+require(['config'], function () {
 
-
-requirejs.config({
-    paths: {
-        "jquery": "/components/jquery/dist/jquery"
-    }
-});
-
-
-require(['jquery'], function ($) {
+    'use strict';
 
     var app = {
         initialize: function () {
-
-            debugger;
-
             // Your code here
 
-            // FIXME : Move this code or use a better plugin
-            // Simple jQuery snippet to convert form labels into inline placeholders
-            // Use this : http://webdesign.tutsplus.com/articles/implementing-the-float-label-form-pattern--webdesign-16407
-            //  	      $("form :input").each(function(index, elem) {
-            //    var eId = $(elem).attr("id");
-            //    var label = null;
-            //    if (eId && (label = $(elem).parents("form").find("label[for="+eId+"]")).length == 1) {
-            //        $(elem).attr("placeholder", $(label).html());
-            //        $(label).remove();
-            //    }
-            // });
+            require(['jquery', 'typeahead'], function ($) {
 
-            // For meetings view, set the selected user's email, phone number & fullname in respective hidde fields
+                // require js file corresponding to current template
+                var viewName = $('body').data('view-name') || false;
 
-            var $invitee = null;
-            if ($(".coaches").size() > 0) {@
-                $invitee = $(".coaches option:selected")
-            }
-            if ($(".students").size() > 0) {@
-                $invitee = $(".students option:selected")
-            }
+                var context = $('body').data('context') || "{}";
 
-            $("#invitedEmail").val($invitee.attr("email"));
-            $("#invitedName").val($invitee.attr("fullName"))
-            $("#invitedPhone").val($invitee.attr("phone"))
 
-            // not a good logic to put this stuff in client side,
-            // ideal case is pass the id to mongodb, and let the backend get the info required.
+                // For meetings view, set the selected user's email, phone number & fullname in respective hidde fields
 
+                // var $invitee = null;
+                // if ($(".coaches").size() > 0) {
+                //     $invitee = $(".coaches option:selected")
+                // }
+                // if ($(".students").size() > 0) {
+                //     $invitee = $(".students option:selected")
+                // }
+
+                // $("#invitedEmail").val($invitee.attr("email"));
+                // $("#invitedName").val($invitee.attr("fullName"))
+                // $("#invitedPhone").val($invitee.attr("phone"))
+
+                // not a good logic to put this stuff in client side,
+                // ideal case is pass the id to mongodb, and let the backend get the info required.
+
+
+                var substringMatcher = function (strs) {
+                    return function findMatches(q, cb) {
+                        var matches, substrRegex;
+
+                        // an array that will be populated with substring matches
+                        matches = [];
+
+                        // regex used to determine if a string contains the substring `q`
+                        substrRegex = new RegExp(q, 'i');
+
+                        // iterate through the pool of strings and for any string that
+                        // contains the substring `q`, add it to the `matches` array
+                        $.each(strs, function (i, str) {
+                            if (substrRegex.test(str)) {
+                                // the typeahead jQuery plugin expects suggestions to a
+                                // JavaScript object, refer to typeahead docs for more info
+                                matches.push({
+                                    value: str
+                                });
+                            }
+                        });
+
+                        cb(matches);
+                    };
+                };
+
+                var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+                    'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+                    'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+                    'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+                    'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+                    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+                    'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+                    'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+                    'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+                ];
+
+                $('#bloodhound .typeahead').typeahead({
+                    hint: true,
+                    highlight: true,
+                    minLength: 1
+                }, {
+                    name: 'states',
+                    displayKey: 'value',
+                    source: substringMatcher(states)
+                });
+
+            });
         }
     };
 
     app.initialize();
+
 
 });
---
+++ public/js/config.js
@@ -0,0 +1,39 @@
+(function () {
+    "use strict";
+
+    requirejs.config({
+        baseURL: '/js',
+        // urlArgs: 'bust=' + new Date(),
+        paths: {
+            'backbone': 'lib/backbone',
+            'underscore': 'lib/underscore',
+            'jquery': 'lib/jquery-1.11.1.min', //for IEx compatibility
+            'typeahead': 'lib/typeahead.bundle.min',
+            'bootstrap': 'lib/bootstrap.min',
+            'dust': 'lib/dust-full.min',
+            'dustHelpers': 'lib/dust-helpers.min',
+        },
+        shim: {
+            'dust-helpers': {
+                deps: ['dustjs']
+            },
+            'dust': {
+                exports: 'Dust'
+            },
+            'underscore': {
+                exports: '_'
+            },
+            'backbone': {
+                exports: 'Backbone'
+            },
+            'bootstrap': {
+                deps: ['jquery']
+            },
+            'typeahead': {
+                deps: ['jquery']
+            }
+        },
+        waitSeconds: 15
+    });
+
+})();
